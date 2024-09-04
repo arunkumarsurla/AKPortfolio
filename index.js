b6e98bb01257dcfca5d15d6cf1dd96b613339186
unknown <arunkumar.surla@phenom.com>
2024-09-04 21:42:06 +0530

    api integrations

--- index.js
+++ index.js
@@ -146,3 +146,108 @@ function myFunction() {
   var scrolled = (winScroll / height) * 100;
   document.getElementById("myBar").style.width = scrolled + "%";
 }
+
+function showLoading() {
+  document.getElementById("loading").style.display = "block";
+}
+
+// Function to hide the loading message
+function hideLoading() {
+  document.getElementById("loading").style.display = "none";
+}
+
+const API_URL =
+  "https://668d0469099db4c579f16037.mockapi.io/api/data/projectData";
+
+const fetchApi = async () => {
+  showLoading();
+  try {
+    const response = await fetch(API_URL);
+    const jsonData = await response.json();
+    displayData(jsonData);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+fetchApi();
+
+function displayData(projectsData) {
+  const projectsDataDiv = document.getElementById("projectsData");
+
+  let htmlString = "";
+
+  projectsData.forEach((project) => {
+    htmlString += `
+         <div class="col-12 col-md-6 col-lg-3" data-aos="fade-up" data-aos-delay="100">
+    <div class="project-section shadow p-3 mb-3">
+        <img src="${project.projectImg}" alt="${project.projectName}" class="project-image w-100" />
+        <h1 class="project-title">${project.projectName}</h1>
+        <div class="d-flex justify-content-between">
+            <a href="${project.projectUrl}" class="project-link" target="_blank">
+                                View Project
+                                <svg width="16px" height="16px" viewBox="0 0 16 16" class="bi bi-arrow-right-short"
+                                    fill="#ff4533" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd"
+                                        d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z" />
+                                </svg>
+                            </a>
+            <img src="${project.logoPng}" class="project-logo" alt="${project.Name}">
+        </div>
+    </div>
+</div>
+`;
+  });
+
+  projectsDataDiv.innerHTML = htmlString;
+}
+
+// async function getData() {
+//   const url = "";
+//   try {
+//     const response = await fetch(url);
+//     if (!response.ok) {
+//       throw new Error(`Response status: ${response.status}`);
+//     }
+
+//     const json = await response.json();
+//     console.log(json);
+//   } catch (error) {
+//     console.error(error.message);
+//   }
+// }
+
+
+
+
+const fetchSkillsData = async () =>{
+  showLoading();
+  const skillsResponse  = await fetch('./skills.json');
+  const jsonSkillsData =  await skillsResponse.json();
+  console.log(jsonSkillsData)
+  displaySkillsData(jsonSkillsData);
+}
+
+fetchSkillsData();
+
+function displaySkillsData(skillsData) {
+  const skillsDataDiv = document.getElementById("skillDataContainer");
+
+  let htmlString = "";
+
+  skillsData.forEach((skills) => {
+    htmlString += `
+      <div class="skill-bar mt-2">
+          <div class="info">
+          <i class="${skills.icon}"></i>
+              <p>${skills.name}</p>
+              <P>${skills.percentage}</P>
+          </div>
+          <div class="bar">
+              <span class="${skills.className}"></span>
+          </div>
+      </div>
+`;
+  });
+  skillsDataDiv.innerHTML = htmlString;
+}
